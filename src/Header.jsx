c5751df5f7b6c655e75e507fa201b4cb2ade8187
barismalkoc <barisberkemalkoc@gmail.com>
2022-02-04 22:17:38 +0300

    amazon-navbar added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
 function Header() {
     return (
         <div className="header">
-            
+            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+
+            <div className="header_search">
+                <input className="header_searchInput" type="text"></input>
+                <SearchIcon className="header_searchIcon"></SearchIcon>
+            </div>
+
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="header_optionLineOne">Hello Guest</span>
+                    <span className="header_optionLineTwo">Sign In</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">Returns</span>
+                    <span className="header_optionLineTwo">& Orders</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineTwo">Prime</span>
+                </div>
+
+            </div>
         </div>
     )
 }
