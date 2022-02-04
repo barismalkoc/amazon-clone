479e822d1a231f24801e501c2ce1c334fc6b791b
barismalkoc <barisberkemalkoc@gmail.com>
2022-02-04 20:32:40 +0300

    header component created

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNAg6-TPj4VMd3PgK8MnIepus5EsFODWQ",
+    authDomain: "challenge-2c96a.firebaseapp.com",
+    projectId: "challenge-2c96a",
+    storageBucket: "challenge-2c96a.appspot.com",
+    messagingSenderId: "1072246980605",
+    appId: "1:1072246980605:web:62b1c7e0db447a5cdce394",
+    measurementId: "G-2DB7RWEF96"
+  };
